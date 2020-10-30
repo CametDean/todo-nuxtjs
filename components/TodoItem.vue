db36f8dcb9a75a7335040d6d7c0973b60a71caa0
Camille Toulouse <camille.toulouse@octo.com>
2020-10-30 16:33:05 +0100

    modif css

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -3,10 +3,22 @@
         <li><input v-model="currentLabel" type="text" v-on:keyup.enter="updateItem"></li>
     </div>
     <div v-else-if="editedItem === 'edited'">
-        <li v-on:dblclick="itemIsBeingEdited" id="id">{{ newLabel }} || <button v-on:click.prevent="deleteTodoItem">Delete</button></li>
+        <li id="id">
+            {{ newLabel }}
+            <div class="buttons">
+                <button class="edition" v-on:click.prevent="itemIsBeingEdited">Edit</button>
+                <button class="suppression" v-on:click.prevent="deleteTodoItem">Delete</button>
+            </div>
+        </li>
     </div>
     <div v-else>
-        <li v-on:dblclick="itemIsBeingEdited" id="id">{{ text }} || <button v-on:click.prevent="deleteTodoItem">Delete</button></li>
+        <li id="id">
+            {{ text }}
+            <div class="buttons">
+                <button class="edition" v-on:click.prevent="itemIsBeingEdited">Edit</button>
+                <button class="suppression" v-on:click.prevent="deleteTodoItem">Delete</button>
+            </div>
+        </li>
     </div>
 </template>
 
@@ -41,11 +53,15 @@ export default {
 
 <style>
     li{
+        width: 100%;
         list-style: none;
         padding-top: 5px;
         padding-bottom: 5px;
         font-size: 20px;
         font-family: 'Roboto', sans-serif;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
     }
 
     input{
@@ -53,6 +69,20 @@ export default {
         padding-bottom: 5px;
         font-size: 20px;
         font-family: 'Roboto', sans-serif;
+    }
+
+    .edition{
+        background: rgb(134, 189, 134);
+        border: none;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    .suppression{
+        background: rgb(221, 145, 145);
+        border: none;
+        padding-top: 5px;
+        padding-bottom: 5px;
     }
 
 </style>
